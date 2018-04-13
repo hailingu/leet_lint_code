553ab8c184e3e7c4113d8102a79b01dcc2188392
hailin <hailin.gu@hypers.com>
2018-04-13 14:29:26 +0800

    Add solution to lint code problem 2

--- leet_lint_code/lint_code/trailing_zeros.cpp
+++ leet_lint_code/lint_code/trailing_zeros.cpp
@@ -20,6 +20,20 @@ class TrailingZeros {
      }
      return total;
    }
+
+   long long trailingZeros2(long long n) {
+     long long total = 0;
+     double t = (double)n;
+     long long base = 2;
+     long long tmp = 0;
+     do {
+       t = t / 10.0;
+       tmp = (long long) (t*base);
+       total = total + tmp;
+       base = base * 2;
+     } while(tmp > 1);
+     return total;
+   }
 };
 
 #endif // LEETLINTCODE_LINTCODE_TRAILINGZEROS_CPP
