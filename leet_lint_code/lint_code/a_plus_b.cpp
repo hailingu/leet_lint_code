03570872d11cdeab11885f4f2d8e8f043d181a78
hailin <hailin.gu@hypers.com>
2018-04-13 14:11:04 +0800

    Lint code 1. A + B Problem

--- leet_lint_code/lint_code/a_plus_b.cpp
+++ leet_lint_code/lint_code/a_plus_b.cpp
@@ -1,9 +1,29 @@
-//
-//  a_plus_b.cpp
-//  leet_lint_code
-//
-//  Created by Hailin Gu on 2018/4/13.
-//  Copyright © 2018 Hailin Gu. All rights reserved.
-//
+// Copyright © 2018 Hailin Gu. All rights reserved.
+// License(GPL)
+// Author: Hailin Gu
+// This is a answer of lint code problem 1.
+#ifndef LEETLINTCODE_LINTCODE_APLUSB_CPP
+#define LEETLINTCODE_LINTCODE_APLUSB_CPP
 
 #include <stdio.h>
+
+
+class APlusB {
+public:
+    int aplusb(int a, int b) {
+        int t = a;
+        a = a ^ b;
+        b = t & b;
+        
+        while (b != 0){
+            t = a;
+            a = (b << 1) ^ a;
+            b = (b << 1) & t;
+        }
+        
+        return a;
+    }
+};
+
+
+#endif // LEETLINTCODE_LINTCODE_APLUSB_CPP
