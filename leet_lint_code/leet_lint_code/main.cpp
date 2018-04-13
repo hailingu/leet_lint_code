1ee2ada235f8884214e3e494db9eee9f6455e927
hailin <hailin.gu@hypers.com>
2018-04-13 14:10:17 +0800

    first commit

---
+++ leet_lint_code/leet_lint_code/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  leet_lint_code
+//
+//  Created by Hailin Gu on 2018/4/13.
+//  Copyright © 2018 Hailin Gu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ leet_lint_code/lint_code/a_plus_b.cpp
@@ -0,0 +1,9 @@
+//
+//  a_plus_b.cpp
+//  leet_lint_code
+//
+//  Created by Hailin Gu on 2018/4/13.
+//  Copyright © 2018 Hailin Gu. All rights reserved.
+//
+
+#include <stdio.h>
