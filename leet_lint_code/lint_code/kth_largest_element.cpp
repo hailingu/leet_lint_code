5c3ac4378a1b4bd1a862dbfa8b83c6d36696b2a2
hailin <hailin.gu@hypers.com>
2018-04-24 11:57:16 +0800

    Add answer to lint code problem 5

---
+++ leet_lint_code/lint_code/kth_largest_element.cpp
@@ -0,0 +1,58 @@
+// Copyright © 2018 Hailin Gu. All rights reserved.
+// License(GPL)
+// Author: Hailin Gu
+// This is a answer of lint code problem 5.
+
+#ifndef LEETLINTCODE_LINTCODE_KTHLARGESTELEMENT_CPP
+#define LEETLINTCODE_LINTCODE_KTHLARGESTELEMENT_CPP
+
+#include <vector>
+
+class KthLargestElement {
+ public:
+   int kthLargestElement(int n, std::vector<int> &nums) {
+     quick_sort(0, (int)nums.size()-1, nums);
+     return nums[nums.size() - n];
+   }
+
+   void quick_sort(int begin, int end, std::vector<int> &nums) {
+     if (begin >= end) return ;
+     int pivoid = nums[end];
+
+     int i = begin, j = end - 1;
+     while (i < j) {
+       while (nums[i] <= pivoid && i < j) ++ i;
+       while (nums[j] >= pivoid && i < j) -- j;
+       swap(nums[i], nums[j]);
+     }
+
+     if (nums[i] > nums[end]) swap(nums[i], nums[end]);
+     quick_sort(begin, i - 1, nums);
+     quick_sort(i + 1, end, nums);
+  }
+
+  void swap(int &a, int &b) {
+    int tmp = a;
+    a = b;
+    b = tmp;
+  }
+
+  void quick_sort2(int begin, int end, std::vector<int> &nums) {
+    if (begin >= end) return ;
+    int pivoid = nums[end];
+
+    int i = begin, j = end;
+    while (i < j) {
+      while (nums[i] <= pivoid && i < j) ++ i;
+      nums[j] = nums[i];
+      while (nums[j] >= pivoid && i < j) -- j;
+      nums[i] = nums[j];
+    }
+
+    nums[j] = pivoid;
+    quick_sort(begin, i - 1, nums);
+    quick_sort(i + 1, end, nums);
+  }
+};
+
+#endif // LEETLINTCODE_LINTCODE_DIGITCOUNTS_CPP
