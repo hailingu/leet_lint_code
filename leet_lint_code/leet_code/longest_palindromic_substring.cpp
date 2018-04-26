12702cb259e7209135948956058317b2a167c0b0
hailin <hailin.gu@hypers.com>
2018-04-26 16:11:31 +0800

    Add answer to leet code problem 6.

---
+++ leet_lint_code/leet_code/longest_palindromic_substring.cpp
@@ -0,0 +1,65 @@
+// Copyright © 2018 Hailin Gu. All rights reserved.
+// License(GPL)
+// Author: Hailin Gu
+// This is a answer of leet code problem 5.
+
+#ifndef LEETLINTCODE_LINTCODE_LONGESTPALINDROMICSUBSTRING_H
+#define LEETLINTCODE_LINTCODE_LONGESTPALINDROMICSUBSTRING_H
+
+#include <vector>
+#include <iostream>
+#include <string.h>
+
+class LongestPalindromicSubstring {
+ public:
+   std::string longestPalindrome(std::string s) {
+     return manacher(s);
+   }
+
+   std::string manacher(std::string s) {
+     std::vector<int> p;
+     std::string s_cpy = "$";
+     for (int i = 0;  i < s.length(); ++ i){
+       s_cpy.append("#");
+       s_cpy.append(s.substr(i, 1));
+     }
+     s_cpy.append("#");
+
+     for (int i = 0; i < s_cpy.length(); ++ i) p.push_back(0);
+
+     int i = 1;
+     int center = 0;
+     int right_border = 0;
+     while (i < s_cpy.length()) {
+       if (i < right_border)
+         p[i] = p[2 * center - i] < right_border - i ?
+         p[2 * center - i] : right_border - i;
+         else p[i] = 1;
+
+       while (s_cpy[i - p[i]] == s_cpy[i + p[i]]) ++ p[i];
+         if (right_border < i + p[i]) {
+           center = i;
+           right_border = i + p[i];
+          }
+        ++ i;
+      }
+
+      int index = 0;
+      int length = 0;
+      for (int i = 0; i < p.size(); ++ i) {
+        if (p[i] > length) {
+          index = i;
+          length = p[i];
+        }
+      }
+
+      std::string ret = "";
+      for (int j = index - length + 1; j < index + length; ++ j)
+        if (s_cpy[j] != '#') ret.append(s_cpy.substr(j, 1));
+
+
+      return ret;
+    }
+};
+
+#endif // LEETLINTCODE_LINTCODE_LONGESTPALINDROMICSUBSTRING_H
--- leet_lint_code/leet_code/median_of_two_sorted_arrays.cpp
+++ leet_lint_code/leet_code/median_of_two_sorted_arrays.cpp
@@ -13,36 +13,39 @@ class MedianOfTwoSortedArrays {
    double findMedianSortedArrays(std::vector<int>& nums1, std::vector<int>& nums2) {
      std::vector<int> a;
      std::vector<int> b;
-     if (nums1.size() > nums2.size())
-       a = nums2, b = nums1;
-     else
-       a = nums1, b = nums2;
+     if (nums1.size() > nums2.size()){
+       a = nums2;
+       b = nums1;
+     } else {
+         a = nums1;
+         b = nums2;
+     }
 
-     int min = 0; int max = a.size();
-     int i = 0;
-     int max_of_left = 0;
-     int min_of_right = 0;
-     int half = (a.size() + b.size() + 1) / 2;
+     int min = 0, i = 0, j = 0;
+     unsigned long max = a.size();
+     int max_of_left = 0, min_of_right = 0;
+     unsigned long half = (a.size() + b.size() + 1) / 2;
      while (min <= max) {
-       i = (min + max) / 2;
-       j = half - i;
+       i = (int)(min + max) / 2;
+       j = (int)half - i;
 
-       if (i < m && b[j - 1] > a[i]) min = i + 1;
+       if (i < a.size() && b[j - 1] > a[i]) min = i + 1;
        else if (i > 0 && a[i - 1] > b[j]) max = i - 1;
        else {
          if (i == 0) max_of_left = b[j - 1];
          else if (j == 0) max_of_left = a[i - 1];
          else max_of_left = b[j - 1] > a[i - 1] ? b[j - 1] : a[i - 1];
 
-         if ((a.size() + b.size()) % 2 == 1) return max_of_left;
+         if ((a.size() + b.size()) % 2 == 1)return max_of_left;
 
-         if (i == m) min_of_right = b[j];
-         else if (j == n) min_of_right = a[i];
+         if (i == a.size()) min_of_right = b[j];
+         else if (j == b.size()) min_of_right = a[i];
          else min_of_right = b[j] < a[i] ? b[j] : a [i];
 
          return (max_of_left + min_of_right) / 2.0;
        }
      }
+     return -1;
    }
 };
 
