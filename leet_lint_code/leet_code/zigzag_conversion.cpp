498f5b5b090f0b7715fb0d5d1899d415fbb7c40e
hailin <hailin.gu@hypers.com>
2018-04-27 15:15:05 +0800

    Update

--- leet_lint_code/leet_code/zigzag_conversion.cpp
+++ leet_lint_code/leet_code/zigzag_conversion.cpp
@@ -6,13 +6,17 @@
 #ifndef LEETLINTCODE_LINTCODE_ZIGZAGCONVERSION_CPP
 #define LEETLINTCODE_LINTCODE_ZIGZAGCONVERSION_CPP
 
+#include <iostream>
+#include <math.h>
+
 class ZigZagConversion {
  public:
    std::string convert(std::string s, int nRows) {
      std::string ret = "";
-     int level = 0, internal = 0, inner_loop = 0, len = s.size();
-     if (0 != nRows - 1) internal = 2*nRows - 2;
-     else internal = 1;
+     int level = 0, internal = 0, inner_loop = 0;
+     size_t len = s.size();
+     if (1 == nRows) internal = 1;
+     else internal = (nRows - 1) << 1;
 
      while (level < nRows) {
        inner_loop = level;
@@ -23,14 +27,47 @@ class ZigZagConversion {
            inner_loop = inner_loop + internal;
          }
         } else {
-          int temp_interal = internal - 2*level;
+          int temp_interal = internal - 2 * level;
           while (inner_loop + temp_interal < len) {
             ret = ret + s[inner_loop + temp_interal];
             inner_loop = inner_loop + temp_interal;
             temp_interal = internal - temp_interal;
           }
         }
-        level ++;
+        ++ level;
+      }
+      return ret;
+    }
+
+    // O(n)
+    std::string convert2(std::string s, int nRows) {
+      int divisor = (nRows - 1) << 1, current_index = 0;
+      if (divisor == 0) return s;
+      std::string ret = "";
+      size_t len = s.size();
+      int index1 = 0, index2 = 0, remainder1 = 0, remainder2 = 0;
+      while (ret.size() != len) {
+        remainder1 = current_index % divisor;
+        remainder2 = (divisor - remainder1) % divisor;
+        index1 = remainder1;
+        if (remainder1 == remainder2) {
+          while (index1 < len) {
+            ret.append(s.substr(index1, 1));
+            index1 += divisor;
+          }
+        } else {
+          index2 = remainder2;
+          while (index1 < len || index2 < len) {
+            if (index1 < index2) {
+              ret.append(s.substr(index1, 1));
+              index1 += divisor;
+            } else {
+              ret.append(s.substr(index2, 1));
+              index2 += divisor;
+            }
+          }
+        }
+      ++ current_index;
       }
       return ret;
     }
