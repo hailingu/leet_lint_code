52edc55fedc0cbd90f057588fbb7a05f2332d595
hailin <hailin.gu@hypers.com>
2018-05-03 02:35:26 +0800

    Add  answer to lint code problem 366

---
+++ leet_lint_code/lint_code/fibonacci.cpp
@@ -0,0 +1,25 @@
+// Copyright © 2018 Hailin Gu. All rights reserved.
+// License(GPL)
+// Author: Hailin Gu
+// This is a answer of lint code problem 366.
+
+#ifndef LEETLINTCODE_LINTCODE_FIBONACCI_CPP
+#define LEETLINTCODE_LINTCODE_FIBONACCI_CPP
+
+#include <vector>
+
+class Fibonacci {
+ public:
+   int fibonacci(int n) {
+     if (n == 1) return 0;
+     if (n == 2) return 1;
+     std::vector<int> v;
+     v.push_back(0);
+     v.push_back(1);
+     int i = 1, j = 0;
+     while(i < n) v.push_back(v[i++] + v[j++]);
+     return v[n-1];
+    }
+};
+
+#endif // LEETLINTCODE_LINTCODE_FIBONACCI_CPP
