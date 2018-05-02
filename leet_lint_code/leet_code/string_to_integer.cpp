0e0f5b221d357136ceebfaac95630dd91127d6eb
hailin <hailin.gu@hypers.com>
2018-05-02 23:39:38 +0800

    Add answer to leet code problem 8

---
+++ leet_lint_code/leet_code/string_to_integer.cpp
@@ -0,0 +1,50 @@
+// Copyright © 2018 Hailin Gu. All rights reserved.
+// License(GPL)
+// Author: Hailin Gu
+// This is a answer of leet code problem 8.
+
+#ifndef LEETLINTCODE_LINTCODE_STRINGTOINTEGER_CPP
+#define LEETLINTCODE_LINTCODE_STRINGTOINTEGER_CPP
+
+#include <iostream>
+
+class StringToInteger {
+ public:
+   int myAtoi(std::string str) {
+     unsigned int zero = 0;
+     int MAX = (~zero) >> 1;
+     int MIN = MAX + 1;
+     int len = (int)str.length();
+     int ret = 0, sign = 1, index = 0;
+     if (0 == len ) return  ret;
+     bool begin = false;
+     while (index < len) {
+       if (str[index] != ' ' &&
+           str[index] != '+' &&
+           str[index] != '-' &&
+           !(str[index] >= '0' && str[index] <= '9'))  break;
+
+      if (!begin) {
+        if (' ' == str[index ++ ]) continue;
+        if ('+' == str[index] || '-' == str[index]) {
+          beginning = true;
+          if ('-' == str[index ++ ]) sign = -1;
+          continue;
+        }
+      }
+
+      if (begin && (str[index] < '0' || str[index] > '9')) break;
+      if (ret > MAX / 10) return MAX;
+      if (ret < MIN / 10) return MIN;
+
+      begin = true;
+      ret = ret * 10  + sign * (str[index ++] - '0');
+     }
+
+     if (sign == 1 && ret < 0) return MAX;
+     if (sign == -1 && ret > 0) return MIN;
+     return ret;
+   }
+};
+
+#endif // LEETLINTCODE_LINTCODE_STRINGTOINTEGER_CPP
