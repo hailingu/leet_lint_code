ba926e8e0296e87bcb65806cd3bfb2abb737706d
hailin <hailin.gu@hypers.com>
2018-05-08 10:51:55 +0800

    Update

--- leet_lint_code/lint_code/LFU_cache.cpp
+++ leet_lint_code/lint_code/LFU_cache.cpp
@@ -1,4 +1,4 @@
-// Copyright © 2018 Hailin Gu. All rights reserved.
+// Copyright Â© 2018 Hailin Gu. All rights reserved.
 // License(GPL)
 // Author: Hailin Gu
 // This is a answer of lint code problem 24.
@@ -10,90 +10,93 @@
 #include <map>
 
 class Node {
- public:
-   Node(int key, int value, int sid) {
-     this->key = key;
-     this->value = value;
-     this->sid = sid;
-     this->next = NULL;
-     this->prev = NULL;
-     this->count = 1;
-   }
-
-   Node* next;
-   Node* prev;
-   int count;
-   int sid;
-   int key;
-   int value;
+public:
+  Node(int key, int value, int sid) {
+    this->key = key;
+    this->value = value;
+    this->sid = sid;
+    this->next = NULL;
+    this->prev = NULL;
+    this->count = 1;
+  }
+  
+  Node* next;
+  Node* prev;
+  int count;
+  int sid;
+  int key;
+  int value;
 };
 
 
 class LFUCache {
- public:
-   LFUCache(int capacity) {
-     this->capacity = capacity;
-     this->head = new Node(-1, -1, -1);
-     this->tail = new Node(-1, -1, -1);
-     this->head->next = this->tail;
-     this->tail->prev = this->head;
-   }
-
-   void set(int key, int value) {
-     if (key_to_node.find(key) != key_to_node.end()) {
-       key_to_node[key]->sid = sid ++;
-       ++ key_to_node[key]->count;
-       key_to_node[key]->value = value;
-     } else if (this->key_to_node.size() == this->capacity){
-       key_to_node.erase(key_to_node.find(this->head->next->key));
-       this->head->next->key = key;
-       this->head->next->value = value;
-       this->head->next->sid = sid ++ ;
-       this->head->next->count = 1;
-       key_to_node[key] = this->head->next;
-     } else {
-       Node* tmp = new Node(key, value, sid ++);
-       this->head->next->prev = tmp;
-       tmp->next = head->next;
-       this->head->next = tmp;
-       tmp->prev = this->head;
-       key_to_node[key] = tmp;
-     }
-     update(key_to_node[key]);
-   }
-
-   void update(Node* node) {
-     Node* iter = node->next;
-     while (iter != this->tail) {
-       if ((node->count == iter->count && node->sid > iter->sid) ||
-            node->count > iter->count ) {
-         iter = iter->next;
-       } else break;
-     }
-
-     node->prev->next = node->next;
-     node->next->prev = node->prev;
-     iter->prev->next = node;
-     node->next = iter;
-     node->prev = iter->prev;
-     iter->prev =  node;
-   }
-
-   int get(int key) {
-     if (key_to_node.find(key) != key_to_node.end()) {
-       key_to_node[key]->count ++;
-       key_to_node[key]->sid = sid ++;
-       update(key_to_node[key]);
-       return key_to_node[key]->value;
-     }
-     return -1;
-   }
-
-   int capacity;
-   int sid;
-   Node* head;
-   Node* tail;
-   std::map<int, Node*> key_to_node;
+public:
+  LFUCache(int capacity) {
+    this->capacity = capacity;
+    this->head = new Node(-1, -1, -1);
+    this->tail = new Node(-1, -1, -1);
+    this->head->next = this->tail;
+    this->tail->prev = this->head;
+    this->size = 0;
+  }
+  
+  void set(int key, int value) {
+    if (key_to_node[key]) {
+      key_to_node[key]->sid = sid ++;
+      ++ key_to_node[key]->count;
+      key_to_node[key]->value = value;
+    } else if (this->size == this->capacity){
+      key_to_node[this->head->next->key] = NULL;
+      this->head->next->key = key;
+      this->head->next->value = value;
+      this->head->next->sid = sid ++ ;
+      this->head->next->count = 1;
+      key_to_node[key] = this->head->next;
+    } else {
+      Node* tmp = new Node(key, value, sid ++);
+      this->head->next->prev = tmp;
+      tmp->next = head->next;
+      this->head->next = tmp;
+      tmp->prev = this->head;
+      key_to_node[key] = tmp;
+      ++ size;
+    }
+    update(key_to_node[key]);
+  }
+  
+  void update(Node* node) {
+    Node* iter = node->next;
+    while (iter != this->tail) {
+      if ((node->count == iter->count && node->sid > iter->sid) ||
+          node->count > iter->count ) {
+        iter = iter->next;
+      } else break;
+    }
+    
+    node->prev->next = node->next;
+    node->next->prev = node->prev;
+    iter->prev->next = node;
+    node->next = iter;
+    node->prev = iter->prev;
+    iter->prev = node;
+  }
+  
+  int get(int key) {
+    if (key_to_node[key]) {
+      ++ key_to_node[key]->count;
+      key_to_node[key]->sid = sid ++;
+      update(key_to_node[key]);
+      return key_to_node[key]->value;
+    }
+    return -1;
+  }
+  
+  int capacity;
+  int sid;
+  Node* head;
+  Node* tail;
+  std::map<int, Node*> key_to_node;
+  int size;
 };
 
 #endif // LEETLINTCODE_LINTCODE_LFUCACHE_CPP
