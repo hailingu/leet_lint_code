ec227c8b2d36a2db13410d36a8b26869c142db4d
hailin <hailin.gu@hypers.com>
2018-05-14 01:24:34 +0800

    Answer to lint code problem 33

---
+++ leet_lint_code/lint_code/n_queensii.cpp
@@ -0,0 +1,70 @@
+// Copyright © 2018 Hailin Gu. All rights reserved.
+// License(GPL)
+// Author: Hailin Gu
+// This is a answer of lint code problem 34.
+
+#ifndef LEETLINTCODE_LINTCODE_NQUEENSII_CPP
+#define LEETLINTCODE_LINTCODE_NQUEENSII_CPP
+
+#include <vector>
+#include <iostream>
+#include <math.h>
+
+class NQueensII {
+ public:
+   int totalNQueens(int n) {
+     std::vector<std::vector<std::string> > result;
+     std::vector<std::string> vs;
+     if (1 == n) {
+       vs.push_back("Q");
+       result.push_back(vs);
+     }
+
+     if (n < 4) return result.size();
+     std::string s(n, '.');
+     placeQueen(n, 0, vs, result);
+     return result.size();
+   }
+
+   void placeQueen(int n, int layer, std::vector<std::string> vs, std::vector<std::vector<std::string> > & result) {
+     if (layer == n) {
+       result.push_back(vs);
+       return ;
+     }
+
+     for (int i = 0; i < n; ++ i) {
+       std::string str(n, '.');
+       str[i]='Q';
+       vs.push_back(str);
+
+       if(valid(vs, layer, i))
+          placeQueen(n, layer +1 , vs, result);
+
+       vs.pop_back();
+     }
+   }
+
+   bool valid(std::vector<std::string> &vs, int layer, int col) {
+     int count = 0;
+     for (int i = 0; i <= layer; ++ i) {
+       if (vs[i][col] == 'Q') ++ count;
+       if (count > 1) return false;
+     }
+
+     count = 0;
+     for (int i = layer, j = col; i >=0 && j >= 0; --i, --j) {
+       if (vs[i][j] == 'Q') ++ count;
+       if (count > 1) return false;
+     }
+
+     count = 0;
+     for (int i = layer, j = col; i >= 0 && j <= vs[0].size(); -- i, ++ j) {
+       if (vs[i][j] == 'Q') ++ count;
+       if (count > 1) return false;
+     }
+
+     return true;
+   }
+};
+
+#endif // LEETLINTCODE_LINTCODE_NQUEENSII_CPP
