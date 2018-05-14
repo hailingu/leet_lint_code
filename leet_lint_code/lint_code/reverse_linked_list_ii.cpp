16e3f2ff558712a58761720f868fb365a7e9f0f0
hailin <hailin.gu@hypers.com>
2018-05-14 10:30:44 +0800

    Add answer to lint and leet code reversed linked list

---
+++ leet_lint_code/lint_code/reverse_linked_list_ii.cpp
@@ -0,0 +1,53 @@
+// Copyright © 2018 Hailin Gu. All rights reserved.
+// License(GPL)
+// Author: Hailin Gu
+// This is a answer of lint code problem 35.
+
+#ifndef LEETLINTCODE_LINTCODE_REVERSELINKEDLISTII_CPP
+#define LEETLINTCODE_LINTCODE_REVERSELINKEDLISTII_CPP
+
+#include <iostream>
+
+class ListNode {
+ public:
+   ListNode(int val) {
+     this->val = val;
+     this->next = NULL;
+   }
+
+   int val;
+   ListNode *next;
+};
+
+class ReverseLinkedListII {
+ public:
+   ListNode * reverseBetween(ListNode * head, int m, int n) {
+     if (NULL == head) return head;
+     ListNode* tmp_head = new ListNode(-1);
+     ListNode* iter_head = tmp_head;
+     tmp_head->next = head;
+
+     int i = 0;
+     ListNode* iter = tmp_head;
+     while (i < m) {
+       iter_head = iter;
+       iter = iter->next;
+       ++ i;
+     }
+
+     ListNode* tmp;
+     ListNode* tmp_tail = iter;
+     while (i <= n) {
+       tmp = iter->next;
+       iter->next = iter_head->next;
+       iter_head->next = iter;
+       iter = tmp;
+       ++ i;
+     }
+
+     tmp_tail->next = tmp;
+     return tmp_head->next;
+   }
+};
+
+#endif // LEETLINTCODE_LINTCODE_REVERSELINKEDLISTII_CPP
