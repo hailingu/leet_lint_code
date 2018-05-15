c12e31791e6ee24b1ab4fa0c3de8342757892fa2
hailin <hailin.gu@hypers.com>
2018-05-15 15:12:51 +0800

    Add and update answer to lint code problem

--- leet_lint_code/lint_code/maximum_subarray.cpp
+++ leet_lint_code/lint_code/maximum_subarray.cpp
@@ -10,21 +10,16 @@
 
 class MaximumSubarray {
  public:
-  int maxSubArray(vector<int> &nums) {
-    int max = 0, temp_left = 0, len = (int)nums.size(), i = 0, tmp = 0;
-    while (i < len) {
-      if (0 == i) {
-        if (nums[i] > 0) temp_left = nums[i];
-        max = nums[i];
-      } else {
-        tmp = temp_left + nums[i];
-        if (temp_left > 0)
-          max = max > tmp ? max : tmp;
-        else
-          max = max > nums[i] ? max : nums[i];
-        if (tmp > 0) temp_left = tmp;
-          else temp_left = 0;
-      }
+  int maxSubArray(std::vector<int> &nums) {
+    unsigned int zero = 0;
+    int min = ((~zero) >> 1)  + 1;
+    int max = min, temp_left = 0, len = (int)nums.size(), i = 0, tmp = 0;
+    while (i != len) {
+      tmp = temp_left + nums[i];
+      if (temp_left > 0) max = max > tmp ? max : tmp;
+      else max = max > nums[i] ? max : nums[i];
+      if (tmp > 0) temp_left = tmp;
+      else temp_left = 0;
       ++ i;
     }
     return max;
---
+++ leet_lint_code/lint_code/maximum_subarray_ii.cpp
@@ -0,0 +1,55 @@
+// Copyright © 2018 Hailin Gu. All rights reserved.
+// License(GPL)
+// Author: Hailin Gu
+// This is a answer of lint code problem 42.
+
+#ifndef LEETLINTCODE_LINTCODE_MAXIMUMSUBARRAYII_CPP
+#define LEETLINTCODE_LINTCODE_MAXIMUMSUBARRAYII_CPP
+
+#include <vector>
+
+class MaximumSubarrayII {
+ public:
+   int maxTwoSubArrays(std::vector<int> &nums) {
+     int len = (int)nums.size();
+     unsigned int zero = 0;
+     int MIN = ((~zero) >> 1)  + 1;
+     int max = MIN;
+     if (1 == len) return max;
+     if (2 == len) return nums[0] + nums[1];
+
+     std::vector<int> max_cnt(len, MIN);
+
+     int i = 0, tmp = 0, right = 0, left = MIN, temp_left = 0;
+     while (i != len - 1) {
+       update(temp_left, nums[i], left);
+       right = maxSubArrayRight(nums, len - 1, i + 1, max_cnt, MIN);
+       tmp = left + right;
+       if (tmp > max) max = tmp;
+       ++ i;
+     }
+     return max;
+   }
+
+   inline int maxSubArrayRight(std::vector<int> &nums, int begin, int end,
+     std::vector<int> &max_cnt, int &min) {
+       if (max_cnt[end] != min) return max_cnt[end];
+       int max = min, temp_right = 0, i = begin;
+       while (i != end) {
+         update(temp_right, nums[i], max);
+         max_cnt[i] = max;
+         -- i;
+       }
+       return max;
+    }
+
+  inline void update(int &tmp_direct, int &nums, int &max) {
+    int tmp = tmp_direct + nums;
+    if (tmp_direct > 0) max = max > tmp ? max : tmp;
+    else max = max > nums ? max : nums;
+    if (tmp > 0) tmp_direct = tmp;
+    else tmp_direct = 0;
+  }
+ };
+
+#endif // LEETLINTCODE_LINTCODE_MAXIMUMSUBARRAYII_CPP
