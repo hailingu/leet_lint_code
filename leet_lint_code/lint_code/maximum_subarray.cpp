3c7158284ffae8f75c1d3a4d149847f8d892b701
hailin <hailin.gu@hypers.com>
2018-05-15 16:12:16 +0800

    Update

--- leet_lint_code/lint_code/maximum_subarray.cpp
+++ leet_lint_code/lint_code/maximum_subarray.cpp
@@ -11,15 +11,11 @@
 class MaximumSubarray {
  public:
   int maxSubArray(std::vector<int> &nums) {
-    unsigned int zero = 0;
-    int min = ((~zero) >> 1)  + 1;
-    int max = min, temp_left = 0, len = (int)nums.size(), i = 0, tmp = 0;
+    int max = nums[0], temp_left = 0, len = (int)nums.size(), i = 0, tmp = 0;
     while (i != len) {
-      tmp = temp_left + nums[i];
-      if (temp_left > 0) max = max > tmp ? max : tmp;
-      else max = max > nums[i] ? max : nums[i];
-      if (tmp > 0) temp_left = tmp;
-      else temp_left = 0;
+      temp_left += nums[i];
+      if (temp_left > max) max = temp_left;
+      if (temp_left < 0) temp_left = 0;
       ++ i;
     }
     return max;
--- leet_lint_code/lint_code/maximum_subarray_ii.cpp
+++ leet_lint_code/lint_code/maximum_subarray_ii.cpp
@@ -10,46 +10,43 @@
 
 class MaximumSubarrayII {
  public:
-   int maxTwoSubArrays(std::vector<int> &nums) {
-     int len = (int)nums.size();
-     unsigned int zero = 0;
-     int MIN = ((~zero) >> 1)  + 1;
-     int max = MIN;
-     if (1 == len) return max;
-     if (2 == len) return nums[0] + nums[1];
+  int maxTwoSubArrays(std::vector<int> &nums) {
+    int len = (int)nums.size();
+    unsigned int zero = 0;
+    int MIN = ((~zero) >> 1)  + 1;
+    int max = MIN;
+    if (1 == len) return max;
+    if (2 == len) return nums[0] + nums[1];
 
-     std::vector<int> max_cnt(len, MIN);
+    std::vector<int> max_cnt(len, MIN);
 
-     int i = 0, tmp = 0, right = 0, left = MIN, temp_left = 0;
-     while (i != len - 1) {
-       update(temp_left, nums[i], left);
-       right = maxSubArrayRight(nums, len - 1, i + 1, max_cnt, MIN);
-       tmp = left + right;
-       if (tmp > max) max = tmp;
-       ++ i;
-     }
-     return max;
+    int i = 0, tmp = 0, right = 0, left = MIN, temp_left = 0;
+    while (i != len - 1) {
+      update(temp_left, nums[i], left);
+      tmp = left + maxSubArrayRight(nums, len - 1, i + 1, max_cnt, MIN);
+      if (tmp > max) max = tmp;
+      ++ i;
+    }
+    return max;
+  }
+
+  inline int maxSubArrayRight(std::vector<int> &nums, int begin, int end,
+    std::vector<int> &max_cnt, int &min) {
+      if (max_cnt[end] != min) return max_cnt[end];
+      int max = min, temp_right = 0, i = begin;
+      while (i != end) {
+        update(temp_right, nums[i], max);
+        max_cnt[i] = max;
+        -- i;
+      }
+      return max;
    }
 
-   inline int maxSubArrayRight(std::vector<int> &nums, int begin, int end,
-     std::vector<int> &max_cnt, int &min) {
-       if (max_cnt[end] != min) return max_cnt[end];
-       int max = min, temp_right = 0, i = begin;
-       while (i != end) {
-         update(temp_right, nums[i], max);
-         max_cnt[i] = max;
-         -- i;
-       }
-       return max;
-    }
-
-  inline void update(int &tmp_direct, int &nums, int &max) {
-    int tmp = tmp_direct + nums;
-    if (tmp_direct > 0) max = max > tmp ? max : tmp;
-    else max = max > nums ? max : nums;
-    if (tmp > 0) tmp_direct = tmp;
-    else tmp_direct = 0;
-  }
- };
+ inline void update(int &tmp_direct, int &nums, int &max) {
+   tmp_direct += nums;
+   if (tmp_direct > max) max = tmp_direct;
+   if (tmp_direct < 0) tmp_direct = 0;
+ }
+};
 
 #endif // LEETLINTCODE_LINTCODE_MAXIMUMSUBARRAYII_CPP
