db72d8b388abfb739c5604e6f06f795106100f06
hailin <hailin.gu@hypers.com>
2018-05-28 14:00:35 +0800

    Update

--- leet_lint_code/leet_code/construct_binary_tree_from_inorder_and_postorder_traversal.cpp
+++ leet_lint_code/leet_code/construct_binary_tree_from_inorder_and_postorder_traversal.cpp
@@ -24,38 +24,30 @@ class TreeNode {
 
 class ConstructBinaryTreeFromInorderAndPostorderTraversal {
  public:
-  TreeNode * buildTree(std::vector<int> &inorder, std::vector<int> &postorder) {
-    if (!postorder.size()) return NULL;
-    int len = (int) postorder.size();
-    std::map<int, int> inorder_map;
-    for (int i = 0; i < len; ++i) inorder_map[inorder[i]] = i;
+   TreeNode * buildTree(std::vector<int> &inorder, std::vector<int> &postorder) {
+     if (!postorder.size()) return NULL;
+     int len = (int) postorder.size();
+     std::map<int, int> inorder_map;
+     for (int i = 0; i < len; ++i) inorder_map[inorder[i]] = i;
+     TreeNode* root = buildTreeHelp(inorder, postorder, 0, len - 1, len - 1,inorder_map);
+     return root;
+   }
 
-    TreeNode* root = buildTreeHelp(inorder, postorder, 0, len - 1, inorder_map, postorder_map);
-    return root;
-  }
+   TreeNode * buildTreeHelp(std::vector<int> &inorder, std::vector<int> &postorder,
+                            int i_begin, int i_end, int p_end,
+                            std::map<int, int> &inorder_map) {
 
-  TreeNode * buildTreeHelp(std::vector<int> &inorder, std::vector<int> &postorder,
-                           int p_begin, int p_end,
-                           std::map<int, int> &inorder_map) {
+     if (i_begin > i_end) return NULL;
+     TreeNode* root = new TreeNode(postorder[p_end]);
+     if (i_begin == i_end) return root;
+     int i_root_index = inorder_map[postorder[p_end]];
+     int len = i_end - i_begin + 1;
+     int left_len = i_root_index - i_begin;
 
-    if (p_begin > p_end) return NULL;
-    TreeNode* root = new TreeNode(postorder[p_end]);
-    if (p_begin == p_end) return root;
-    int i_root = inorder_map[postorder[p_end]];
-    int left_index = p_begin - 1;
-    for (int i = p_end - 1; i >= p_begin; -- i) {
-      if (inorder_map[postorder[i]] < i_root) {
-        left_index = i;
-        break;
-      }
-    }
-
-    int right_index = p_end - 1;
-    root->left = buildTreeHelp(inorder, postorder, p_begin, left_index, inorder_map);
-    root->right = buildTreeHelp(inorder, postorder,  left_index + 1, right_index, inorder_map);
-
-    return root;
-  }
+     root->left = buildTreeHelp(inorder, postorder, i_begin, i_root_index - 1, p_end - len + left_len,inorder_map);
+     root->right = buildTreeHelp(inorder, postorder, i_root_index + 1, i_end, p_end - 1, inorder_map);
+     return root;
+   }
 };
 
 #endif // LEETLINTCODE_LINTCODE_CONSTRUCBINARYTREEFROMINORDERANDPOSTORDERTRAVERSAL_CPP
