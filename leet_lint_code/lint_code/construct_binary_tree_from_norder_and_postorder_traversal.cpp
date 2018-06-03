e55cb63a0f37e6787cd64d9e0b7f7d409403f373
Gu Hailin <linhaigu@gmail.com>
2018-06-03 17:27:29 +0800

    update

--- leet_lint_code/lint_code/construct_binary_tree_from_norder_and_postorder_traversal.cpp
+++ leet_lint_code/lint_code/construct_binary_tree_from_norder_and_postorder_traversal.cpp
@@ -9,6 +9,7 @@
 #include <vector>
 #include <map>
 #include <iostream>
+#include <stack>
 
 class ConstructBinaryTreeFromInorderAndPostorderTraversal {
  public:
@@ -18,45 +19,36 @@ class ConstructBinaryTreeFromInorderAndPostorderTraversal {
      TreeNode* root = new TreeNode(postorder[len - 1]);
      std::map<int, int> inorder_map;
      std::map<int, int> postorder_map;
+     std::stack<  TreeNode*> s;
      for (size_t i = 0; i < len; ++i) {
        inorder_map[inorder[i]] = i;
        postorder_map[postorder[i]] = i;
      }
 
-     buildTreeHelp(inorder, postorder, inorder_map, postorder_map, root, 1);
+     int i = len - 2, j = len - 1;
+     TreeNode* iter = root;
+     while (j >= 0) {
+       if (j == i - 1) {
+         if (inorder_map[postorder[j]] > inorder_map[postorder[i]]) {
+           iter->right = new TreeNode(postorder[j]);
+           if (!iter->left || !iter->right) s.push(iter);
+           iter = iter->right;
+           -- j;
+         } else {
+           iter = s.top();
+           s.pop();
+           i = postorder_map[iter->val];
+         }
+       } else if (inorder_map[postorder[j]] < inorder_map[postorder[i]]) {
+         iter->left = new TreeNode(postorder[j]);
+         if (!iter->left || !iter->right) s.push(iter);
+         iter = iter->right;
+         -- j;
+       }
+     }
      return root;
    }
 
-   void buildTreeHelp(std::vector<int> &inorder,
-                      std::vector<int> &postorder,
-                      std::map<int, int> &inorder_map,
-                      std::map<int, int> &postorder_map,
-                      TreeNode* root, int cnt){
-      if (used.size() == inorder.size()) return ;
-      int root_post_index = postorder_map[root->val];
-      int root_inorder_index = inorder_map[root->val];
-      int i = root_post_index - 1;
-      bool found_left = false, found_right = false;
-      while (i >= 0) {
-        if (!found_left &&inorder_map[postorder[i]] < root_inorder_index && used.find(postorder[i]) == used.end()) {
-          root->left = new TreeNode(postorder[i]);
-          used.insert(postorder[i]);
-          found_left = true;
-        }
-
-        if (!found_right && inorder_map[postorder[i]] > root_inorder_index && used.find(postorder[i]) == used.end()) {
-          root->right = new TreeNode(postorder[i]);
-          used.insert(postorder[i]);
-          found_right = true;
-        }
-        -- i;
-      }
-
-      if (root->left)
-        buildTreeHelp(inorder, postorder, inorder_map, postorder_map, root->left, used);
-      if (root->right)
-        buildTreeHelp(inorder, postorder, inorder_map, postorder_map, root->right, used);
-    }
   }
 };
 
