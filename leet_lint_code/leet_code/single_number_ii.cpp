6b7bb23de4fba47eb18bf6b317873a19c28ae0ef
hailin <hailin.gu@hypers.com>
2018-06-04 11:52:33 +0800

    Add answer to leet code

---
+++ leet_lint_code/leet_code/single_number_ii.cpp
@@ -0,0 +1,23 @@
+// Copyright © 2018 Hailin Gu. All rights reserved.
+// License(GPL)
+// Author: Hailin Gu
+// This is a answer of lint code problem 137.
+
+#ifndef LEETLINTCODE_LINTCODE_SIGNLENUMBERII_CPP
+#define LEETLINTCODE_LINTCODE_SIGNLENUMBER_CPP
+
+#include <vector>
+
+class SingleNumberII {
+ public:
+   int singleNumberII(vector<int> &A) {
+       int a = 0, b = 0;
+       for (int i = 0; i < A.size(); ++ i) {
+           b = (b ^ A[i]) & ~a;
+           a = (a ^ A[i]) & ~b;
+       }
+       return b;
+   }
+};
+
+#endif // LEETLINTCODE_LINTCODE_SIGNLENUMBERII_CPP
---
+++ leet_lint_code/leet_code/single_number_iii.cpp
@@ -0,0 +1,29 @@
+// Copyright © 2018 Hailin Gu. All rights reserved.
+// License(GPL)
+// Author: Hailin Gu
+// This is a answer of leet code problem 260.
+
+#ifndef LEETLINTCODE_LINTCODE_SIGNLENUMBERIII_CPP
+#define LEETLINTCODE_LINTCODE_SIGNLENUMBERIII_CPP
+
+#include <vector>
+
+class SingleNumberIII {
+ public:
+   std::vector<int> singleNumberIII(std::vector<int> &A) {
+      int a = 0;
+      std::vector<int> result(2, 0);
+      for (int i = 0; i < A.size(); ++ i)
+        a = a ^ A[i];
+
+      a &= - a;
+      for (int i = 0; i < A.size(); ++ i) {
+        if (a & A[i]) result[0] ^= A[i];
+        else result[1] ^= A[i];
+      }
+
+      return result;
+   }
+};
+
+#endif // LEETLINTCODE_LINTCODE_SIGNLENUMBERIII_CPP
