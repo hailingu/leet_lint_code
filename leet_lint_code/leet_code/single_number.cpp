fe3cf4ead86d46e93823f72ed91b46e0f3c621a8
hailin <hailin.gu@hypers.com>
2018-06-04 11:50:14 +0800

    Add answer to lint code

---
+++ leet_lint_code/leet_code/single_number.cpp
@@ -0,0 +1,22 @@
+// Copyright © 2018 Hailin Gu. All rights reserved.
+// License(GPL)
+// Author: Hailin Gu
+// This is a answer of lint code problem 136.
+
+#ifndef LEETLINTCODE_LINTCODE_SIGNLENUMBER_CPP
+#define LEETLINTCODE_LINTCODE_SIGNLENUMBER_CPP
+
+#include <vector>
+
+class SingleNumber {
+ public:
+   int singleNumber(vector<int> &A) {
+       int single_num = A[0];
+       for (int i = 1; i < A.size(); ++ i) {
+           single_num = single_num ^ A[i];
+       }
+       return single_num;
+   }
+};
+
+#endif // LEETLINTCODE_LINTCODE_SIGNLENUMBER_CPP
