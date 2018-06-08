4b895bf6024cad73975cdcbeced9d479b7e83971
hailin <hailin.gu@hypers.com>
2018-06-08 16:35:34 +0800

    Add answer to leet code

---
+++ leet_lint_code/leet_code/positions_of_large_groups.java
@@ -0,0 +1,23 @@
+// Copyright © 2018 Hailin Gu. All rights reserved.
+// License(GPL)
+// Author: Hailin Gu
+// This is a answer of leet code problem 844.
+
+class PositionsOfLargeGroups {
+  public List<List<Integer>> largeGroupPositions(String S) {
+    int i = 0, j = 1;
+    List<List<Integer>> list = new ArrayList<List<Integer>>();
+    while (i < S.length()) {
+      j = i + 1;
+      while ( j < S.length() && S.charAt(i) == S.charAt(j)) ++ j;
+      if (j >= i + 3) {
+        List<Integer> list1 = new ArrayList<Integer>();
+        list1.add(i);
+        list1.add(j - 1);
+        list.add(list1);
+        }
+      i = j;
+    }
+    return list;
+  }
+}
