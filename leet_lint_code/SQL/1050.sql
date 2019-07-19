cad7d9873fa37f0e254b7b90535c53eca20b0c00
hailingu <linhaigu@gmail.com>
2019-07-19 16:54:50 +0800

    update

--- leet_lint_code/SQL/1050.sql
+++
@@ -1,7 +0,0 @@
-/* Copyright © 2018 Hailin Gu. All rights reserved.
-   License(GPL)
-   Author: Hailin Gu
-   This is a answer of leet code problem 1050.
-   Date: 2019.7.4
-*/
-SELECT actor_id, director_id FROM ActorDirector GROUP BY actor_id, director_id HAVING COUNT(actor_id) > 2;
--- leet_lint_code/SQL/175.sql
+++
@@ -1,1 +0,0 @@
-SELECT p.FirstName As FirstName, p.LastName As LastName, addr.City As City, addr.State AS State FROM Person p LEFT OUTER JOIN Address addr ON p.PersonId = addr.PersonId
