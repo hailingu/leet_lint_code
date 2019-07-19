21fb9b74b857273e7a7ca21aeebc08a032cb8e57
hailingu <linhaigu@gmail.com>
2019-07-20 02:32:26 +0800

    update

---
+++ leet_lint_code/leet_code/java/ReverseWords.java
@@ -0,0 +1,31 @@
+// Copyright Â© 2018 Hailin Gu. All rights reserved.
+// License(GPL)
+// Author: Hailin Gu
+// This is a answer of lint code problem 151.
+// Date: 2019.07.20
+
+class ReverseWords {
+    public String reverseWords(String s) {
+        Stack<String> stack = new Stack<>();
+        String[] splits = s.split(" ");
+        stack.addAll(Arrays.asList(splits));
+        
+        StringBuilder sb = new StringBuilder();
+
+        while (!stack.empty()) {
+            String top = stack.pop();
+            if (top.length() > 0) {
+                sb.append(top);
+                if (sb.charAt(sb.length() - 1) != ' ') {
+                    sb.append(" ");
+                }
+            }
+        }
+        
+        if (sb.length() > 1)
+            return sb.substring(0, sb.length() - 1);
+        else {
+            return "";
+        }
+    }
+}
