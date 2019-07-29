c2801fe52c74cdfbfe90658533e37c4844261b97
Gu Hailin <linhaigu@gmail.com>
2019-07-29 18:27:16 +0800

    Create Subsets.java

---
+++ leet_lint_code/leet_code/java/Subsets.java
@@ -0,0 +1,36 @@
+// Copyright © 2018 Hailin Gu. All rights reserved.
+// License
+// Author: Hailin Gu
+// This is a answer of leet code problem 78.
+// Date: 2019.7.29
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+class Subsets {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        
+    
+        List<List<Integer>> tmp = new ArrayList<>();
+        for (int i : nums) {
+             tmp.clear();
+            for (List list: ans) {
+                List nList = new ArrayList(list);
+                nList.add(i);
+                tmp.add(nList);
+            }
+
+            List<Integer> n = new ArrayList<>();
+            n.add(i);
+            tmp.add(n);
+            ans.addAll(tmp);
+        }
+
+        ans.add(new ArrayList<>());
+        return ans;
+    }
+}
