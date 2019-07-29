2c6c32ecd2380ff2e8bbf9948ad569b88f5b2d9f
Gu Hailin <linhaigu@gmail.com>
2019-07-29 18:33:44 +0800

    Create ArrayPairSum.java

---
+++ leet_lint_code/leet_code/java/ArrayPairSum.java
@@ -0,0 +1,21 @@
+// Copyright © 2018 Hailin Gu. All rights reserved.
+// License
+// Author: Hailin Gu
+// This is a answer of leet code problem 1138.
+// Date: 2019.7.29
+
+
+import java.util.Arrays;
+
+class ArrayPairSum {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        
+        int ans = 0;
+        for (int i = 0; i < nums.length / 2; ++ i) {
+            ans += nums[i*2];
+        }
+        
+        return ans;
+    }
+}
