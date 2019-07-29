962f37de15f226e595eeb26ad388cff13d8205a7
Gu Hailin <linhaigu@gmail.com>
2019-07-29 14:53:41 +0800

    Update MaxIncreaseKeepingSkyline.java

--- leet_lint_code/leet_code/java/MaxIncreaseKeepingSkyline.java
+++ leet_lint_code/leet_code/java/MaxIncreaseKeepingSkyline.java
@@ -5,32 +5,29 @@
 
 public class MaxIncreaseKeepingSkyline {
     public int maxIncreaseKeepingSkyline(int[][] grid) {
-        int [][] minGrid = new int[grid.length][grid[0].length];
+        int[] minGrid = new int[grid.length];
         int sum0 = 0;
         int sum1 = 0;
 
-        for (int m = 0; m < grid.length; ++ m) {
+        for (int m = 0; m < grid.length; ++m) {
             int minMax = grid[m][0];
-            for (int n = 0; n < grid[0].length; ++ n) {
+            for (int n = 0; n < grid[0].length; ++n) {
                 minMax = Math.max(minMax, grid[m][n]);
                 sum0 += grid[m][n];
             }
-
-            for (int n = 0; n < grid[0].length; ++ n) {
-                minGrid[m][n] = minMax;
-            }
+            minGrid[m] = minMax;
             sum1 += minMax * grid[0].length;
         }
 
-        for (int m = 0; m < grid[0].length; ++ m) {
+        for (int m = 0; m < grid[0].length; ++m) {
             int minMax = grid[0][m];
             for (int[] ints : grid) {
                 minMax = Math.max(minMax, ints[m]);
             }
 
-            for (int n = 0; n < grid.length; ++ n) {
-                if (minMax < minGrid[n][m]) {
-                    sum1 += minMax - minGrid[n][m];
+            for (int n = 0; n < grid.length; ++n) {
+                if (minMax < minGrid[n]) {
+                    sum1 += minMax - minGrid[n];
                 }
             }
         }
