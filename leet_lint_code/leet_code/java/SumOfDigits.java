0a72e0c52cf01e3b34fbda4d1746e32aa2a88ab3
Gu Hailin <linhaigu@gmail.com>
2019-07-29 18:38:56 +0800

    Create SumOfDigits.java

---
+++ leet_lint_code/leet_code/java/SumOfDigits.java
@@ -0,0 +1,28 @@
+// Copyright © 2018 Hailin Gu. All rights reserved.
+// License
+// Author: Hailin Gu
+// This is a answer of leet code problem 1085.
+// Date: 2019.7.29
+
+
+class SumOfDigits {
+    public int sumOfDigits(int[] A) {
+        int min = A[0];
+        
+        for (int i : A) {
+            min = min < i ? min : i;
+        }
+        
+        int sum = min % 10;
+        min = min / 10;
+        while (min > 0) {
+            sum += min % 10;
+            min = min / 10;
+        }
+        
+        if (sum % 2 == 1) {
+            return 0;
+        }
+        
+        return 1;
+    }
