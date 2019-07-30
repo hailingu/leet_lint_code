66d0237b323437f123e37c4a4f7cd8bb4091e4e0
Gu Hailin <linhaigu@gmail.com>
2019-07-30 15:17:43 +0800

    Update TwoSum.java

--- leet_lint_code/leet_code/java/TwoSum.java
+++ leet_lint_code/leet_code/java/TwoSum.java
@@ -10,18 +10,17 @@ import java.util.Map;
 class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
-        int[] ret = new int[2];
+        int[] ans = new int[2];
 
         for (int i = 0; i < nums.length; ++i) {
-            int value = target - nums[i];
-            if (map.containsKey(value)) {
-                int index1 = map.get(value);
-                ret[0] = index1 < i ? index1 : i;
-                ret[1] = index1 > i ? index1 : i;
+            if (map.containsKey(nums[i])) {
+                ans[0] = map.get(nums[i]);
+                ans[1] = i;
+                break;
             } else {
-                map.put(nums[i], i);
+                map.put(target - nums[i], i);
             }
         }
-        return ret;
+        return ans;
     }
 }
