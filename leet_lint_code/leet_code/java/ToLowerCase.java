a0dd99cc067bb7844cd21983e72ee0878b0f6d85
Gu Hailin <linhaigu@gmail.com>
2019-08-08 07:26:34 +0800

    Create ToLowerCase.java

---
+++ leet_lint_code/leet_code/java/ToLowerCase.java
@@ -0,0 +1,11 @@
+// Copyright © 2018 Hailin Gu. All rights reserved.
+// License
+// Author: Hailin Gu
+// This is a answer of leet code problem 709.
+// Date: 2019.8.8
+
+class ToLowerCase {
+    public String toLowerCase(String str) {
+        return str.toLowerCase();
+    }
+}
