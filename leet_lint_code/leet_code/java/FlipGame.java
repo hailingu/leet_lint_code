323753f5077022478af59cc895e8225805f2e7bc
hailingu <linhaigu@gmail.com>
2019-08-09 00:48:12 +0800

    Create FlipGame.java

---
+++ leet_lint_code/leet_code/java/FlipGame.java
@@ -0,0 +1,23 @@
+// Copyright © 2018 Hailin Gu. All rights reserved.
+// License
+// Author: leetcode
+// This is a answer of leet code problem 293.
+// Date: 2019.8.9
+
+class FlipGame {
+    public List<String> generatePossibleNextMoves(String s) {
+        List<String> ans = new ArrayList<>();
+        int i = 0;
+        while (i < s.length() - 1) {
+            if (s.charAt(i) == '+' && s.charAt(i + 1) == '+') {
+                StringBuilder sb = new StringBuilder(s);
+                sb.setCharAt(i, '-');
+                sb.setCharAt(i + 1, '-');
+                ans.add(sb.toString());
+            }
+            ++ i;
+        }   
+        return ans;
+    }
+    
+}
