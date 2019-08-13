79e1a9ee88af26ff12d5ef22bc0e282040c23b51
Gu Hailin <linhaigu@gmail.com>
2019-08-13 16:20:04 +0800

    Update RemoveLinkedListElements.java

--- leet_lint_code/leet_code/java/RemoveLinkedListElements.java
+++ leet_lint_code/leet_code/java/RemoveLinkedListElements.java
@@ -4,36 +4,24 @@
 // This is a answer of leet code problem 203.
 // Date: 2019.8.13
 
-import java.util.List;
-import java.util.ArrayList;
 
 class RemoveLinkedListElements {
     public ListNode removeElements(ListNode head, int val) {
-        List<ListNode> list = new ArrayList<>();
-        
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
         ListNode iter = head;
+        ListNode curr = dummyHead;
+        
         while (iter != null) {
             if (iter.val != val) {
-                list.add(iter);
-            }
-            
-            iter = iter.next;
-        }
-        
-        head = null;
-        if (list.size() > 0) {
-            head = list.get(0);
-            iter = head;
-            int i = 1;
-            while (i < list.size()) {
-                iter.next = list.get(i);
                 iter = iter.next;
-                ++ i;
+                curr = curr.next;
+            } else {
+                iter = iter.next;
+                curr.next = iter;
             }
-            iter.next = null;
         }
-    
         
-        return head;
+        return dummyHead.next;
     }
 }
