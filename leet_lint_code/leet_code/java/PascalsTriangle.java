47a531ca8e0ea14c915d9925a4d77d17d47019ad
Gu Hailin <linhaigu@gmail.com>
2019-08-21 17:56:24 +0800

    Create PascalsTriangle.java

---
+++ leet_lint_code/leet_code/java/PascalsTriangle.java
@@ -0,0 +1,39 @@
+// Copyright © 2018 Hailin Gu. All rights reserved.
+// License(GPL)
+// Author: Hailin Gu
+// This is a answer of leet code problem 118.
+// Date: 2019.8.21
+
+
+import java.util.List;
+import java.util.ArrayList;
+
+class PascalsTriangle {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new ArrayList<>();
+        
+        for (int i = 0; i < numRows; ++ i) {
+            List<Integer> levelAns = new ArrayList<>();
+            
+            if (ans.size() == 0) {
+                levelAns.add(1);
+                ans.add(levelAns);
+            } else if (ans.size() == 1) {
+                levelAns.add(1);
+                levelAns.add(1);
+                ans.add(levelAns);
+            } else {
+                levelAns.add(1);
+                
+                List<Integer> prev = ans.get(i - 1);
+                for (int q = 1; q < i; q ++) {
+                    levelAns.add(prev.get(q-1) + prev.get(q));
+                }
+                
+                levelAns.add(1);
+                ans.add(levelAns);
+            }
+        }
+        return ans;
+    }
+}
