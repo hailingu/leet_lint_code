a02852f8985c28719b6328dd10376cbcb8459c71
hailingu <linhaigu@gmail.com>
2019-08-23 03:30:19 +0800

    Create SortArrayByParityII.java

---
+++ leet_lint_code/leet_code/java/SortArrayByParityII.java
@@ -0,0 +1,35 @@
+// Copyright © 2018 Hailin Gu. All rights reserved.
+// License
+// Author: Hailin Gu
+// This is a answer of leet code problem 922.
+// Date: 2019.8.23
+
+
+class SortArrayByParity {
+    public int[] sortArrayByParityII(int[] A) {
+        List<Integer> odd = new ArrayList<>();
+        List<Integer> even = new ArrayList<>();
+      
+        for (int a : A) {
+          if (a %2 == 0) {
+            even.add(a);
+          } else {
+            odd.add(a);
+          }
+        }
+            
+        int i = 0;
+        int m = 0;
+        int n = 0;
+        
+        while (i < A.length) {
+          if ( i %2 == 1) {
+            A[i ++] = odd.get(m ++);
+          } else {
+            A[i ++] = even.get(n ++);
+          }
+        }
+        
+        return A;
+    }
+}
