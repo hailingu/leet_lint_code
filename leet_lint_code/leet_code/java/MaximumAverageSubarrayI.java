1ac23939126a6183314350820a3e09dd0b6089ea
Gu Hailin <linhaigu@gmail.com>
2019-08-23 18:00:29 +0800

    Create MaximumAverageSubarrayI.java

---
+++ leet_lint_code/leet_code/java/MaximumAverageSubarrayI.java
@@ -0,0 +1,29 @@
+// Copyright © 2018 Hailin Gu. All rights reserved.
+// License
+// Author: Hailin Gu
+// This is a answer of leet code problem 643.
+// Date: 2019.8.23
+
+
+class MaximumAverageSubarrayI {
+    public double findMaxAverage(int[] nums, int k) {
+      
+        int c = 0;
+        double ans = Integer.MIN_VALUE;
+        double sum = 0;
+        for (int i = 0; i < nums.length; ++ i) {
+          if (c < k) {
+            sum += nums[i];
+            ++ c;
+          }
+          
+          if (c == k) {
+            ans = Math.max(ans, sum / k);
+            --c; 
+            sum -= nums[i - k + 1];
+          }
+        }
+          
+        return ans;
+    }
+}
